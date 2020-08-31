2d357adf6fa07e3b3ab6c39c408a9bd7ee0331bf
dev-mangal <69078514+dev-mangal@users.noreply.github.com>
2020-08-31 12:34:40 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,ground,rect1,rect2,rect3;
+var packageBody,ground,rect1sprite,rect2sprite,rect3sprite;
+var rect1,rect2,rect3;
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
@@ -20,6 +21,13 @@ function setup() {
 	packageSprite.addImage(packageIMG);
 	packageSprite.scale=0.2;
 
+	rect1sprite=createSprite(400,650,200,20);
+	rect1sprite.shapeColor="red";
+	rect2sprite=createSprite(510,610,20,100);
+	rect2sprite.shapeColor="red";
+	rect3sprite=createSprite(290,610,20,100);
+	rect3sprite.shapeColor="red";
+
 	helicopterSprite=createSprite(width/2, 200, 10,10);
 	helicopterSprite.addImage(helicopterIMG);
 	helicopterSprite.scale=0.6;
@@ -28,7 +36,8 @@ function setup() {
 	groundSprite.shapeColor=color(255);
 
 	rect1=Bodies.rectangle(400,630,200,20);
-	rect1.shapeColor=color(0);
+	rect2=Bodies.rectangle(510,610,20,100);
+	rect3=Bodies.rectangle(290,610,20,100);
 
 	engine = Engine.create();
 	world = engine.world;
@@ -41,6 +50,7 @@ function setup() {
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
  	World.add(world, ground);
 	World.add(world,rect1);
+	World.add(world,rect2);
 
 	Engine.run(engine);
   
@@ -61,7 +71,6 @@ function keyPressed() {
     // Look at the hints in the document and understand how to make the package body fall only on
 	Matter.Body.setStatic(packageBody,false);
 	packageBody.restitution=0.01;
-	
   }
 }
 
